ef56ff7b85182ca904bea726185775feb6780073
Kahf Hussain <kahfnhussain@gmail.com>
2023-04-11 14:45:23 +0200

    Creating layout and attaching API

---
+++ weatherapp/lib/widgets/constants.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  final primaryColor = Color.fromARGB(255, 134, 107, 252);
+  final secondaryColor = const Color(0xffa1c6fd);
+  final tertiaryColor = const Color(0xff205cf1);
+  final blackColor = const Color(0xff000000);
+
+  final greyColor = const Color(0xffd9dadb);
+
+  final linearGradientBlue = const LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.topLeft,
+      colors: <Color>[Color(0xffABcff2), Color.fromARGB(255, 101, 156, 211)],
+      stops: [0.0, 1.0] //10% of the width, so there are ten blinds
+      );
+
+  final linearGradientPurple = const LinearGradient(
+    begin: Alignment.bottomRight,
+    end: Alignment.topLeft,
+    colors: [Color(0xff51087e), Color.fromARGB(255, 120, 1, 195)],
+    stops: [0.0, 1.0], //10% of the width, so there are ten blinds
+  );
+}
